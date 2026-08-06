9521d6670a6fe3e5fa93617cf2b57085dc6d3e86
adamdjbrett <adamdjbrett@users.noreply.github.com>
2026-08-06 11:32:35 -0500

    svg fix

--- src/_config/setup/generate-favicons.js
+++ src/_config/setup/generate-favicons.js
@@ -7,19 +7,22 @@ async function createFavicons() {
   const outputDir = 'src/assets/images/favicon';
   fs.mkdirSync(outputDir, {recursive: true});
 
-  // Get the SVG logo
-  const svgBuffer = fs.readFileSync(pathToSvgLogo);
+  const logoBuffer = fs.readFileSync(pathToSvgLogo);
+  const icon192 = await sharp(logoBuffer).resize(192, 192).png().toBuffer();
 
   // SVG icon
-  fs.writeFileSync(`${outputDir}/favicon.svg`, svgBuffer);
+  fs.writeFileSync(
+    `${outputDir}/favicon.svg`,
+    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 192"><image width="192" height="192" href="data:image/png;base64,${icon192.toString('base64')}"/></svg>\n`
+  );
 
   // PNG icons
-  await sharp(svgBuffer).resize(192, 192).toFile(`${outputDir}/icon-192x192.png`);
-  await sharp(svgBuffer).resize(512, 512).toFile(`${outputDir}/icon-512x512.png`);
-  await sharp(svgBuffer).resize(180, 180).toFile(`${outputDir}/apple-touch-icon.png`);
+  fs.writeFileSync(`${outputDir}/icon-192x192.png`, icon192);
+  await sharp(logoBuffer).resize(512, 512).toFile(`${outputDir}/icon-512x512.png`);
+  await sharp(logoBuffer).resize(180, 180).toFile(`${outputDir}/apple-touch-icon.png`);
 
   // maskable icon
-  await sharp(svgBuffer)
+  await sharp(logoBuffer)
     .resize(512, 512)
     .extend({
       top: 50,
@@ -31,7 +34,7 @@ async function createFavicons() {
     .toFile(`${outputDir}/maskable-icon.png`);
 
   // ICO icon
-  const iconSharp = sharp(svgBuffer);
+  const iconSharp = sharp(logoBuffer);
   await sharpsToIco([iconSharp], `${outputDir}/favicon.ico`, {sizes: [32]});
 
   console.log('All favicons generated.');
